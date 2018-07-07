4d0b54610533e087060aa1bf14a51919a8f722c4
Giorgi Khorguani <gkhorguani@live.com>
2018-07-07 17:12:52 -0500

    Intiial commit

---
+++ GithubBrowser/View/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  GithubBrowser
+//
+//  Created by Giorgi Khorguani on 7/7/18.
+//  Copyright © 2018 Giorgi Khorguani. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .green
+
+        // Do any additional setup after loading the view.
+        print("Login vc has been called")
+    }
+
+}
---
+++ GithubBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubBrowser
+//
+//  Created by Giorgi Khorguani on 7/7/18.
+//  Copyright © 2018 Giorgi Khorguani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
